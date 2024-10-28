24a54f635fc14930ace7a022b170ab7475aa0f7a
clapkong <clapkong@gmail.com>
2024-10-28 20:52:04 +0900

    map 이미지 mappage에 추가,손가락으로 줌인 & 버튼 이용해 줌인하는 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -555,13 +555,76 @@ class MapsPage extends StatefulWidget {
 }
 
 class _MapsPageState extends State<MapsPage> {
+  double _currentScale = 1.0;
+  final TransformationController _transformationController = TransformationController();
+
+  void _zoomIn() {
+    setState(() {
+      _currentScale += 0.2;
+      _updateTransformation();
+    });
+  }
+
+  void _zoomOut() {
+    setState(() {
+      if (_currentScale > 1.0) {
+        _currentScale -= 0.2;
+        _updateTransformation();
+      }
+    });
+  }
+
+   void _updateTransformation() {
+    // Prevent zooming out beyond the initial size and ensure image stays within bounds
+    _transformationController.value = Matrix4.identity()
+      ..scale(_currentScale)
+      ..translate(
+        _transformationController.value.getTranslation().x.clamp(
+            -MediaQuery.of(context).size.width * (_currentScale - 1),
+            MediaQuery.of(context).size.width * (_currentScale - 1)),
+        _transformationController.value.getTranslation().y.clamp(
+            -MediaQuery.of(context).size.height * (_currentScale - 1),
+            MediaQuery.of(context).size.height * (_currentScale - 1)),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Maps Page',
-        style: TextStyle(fontSize: 24),
-      ),
+      child: Stack(
+        children: [
+          InteractiveViewer(
+            transformationController: _transformationController,
+            boundaryMargin: EdgeInsets.zero, // Increased boundary margin to prevent image from going out of bounds
+            minScale: 1.0, // Set minimum scale to prevent zooming out too much
+            child:Image.asset(
+              '../assets/images/map.png',
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width * 2,
+              height: MediaQuery.of(context).size.height * 2,
+            ),
+          ),
+          Positioned(
+            right: 16.0,
+            bottom: 16.0,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  heroTag: "zoomIn",
+                  onPressed: _zoomIn,
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(height: 10),
+                FloatingActionButton(
+                  heroTag: "zoomOut",
+                  onPressed: _zoomOut,
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ),
+        ]
+      )
     );
   }
 }
