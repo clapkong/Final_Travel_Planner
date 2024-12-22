80a2b4a9b548cc603469fdef333a68bf8bf1ad30
clapkong <clapkong@gmail.com>
2024-12-23 01:32:40 +0900

    Search Page에 UserInput Provider 추가 및 데이터 Map<String, dynamic>으로 받아오기 추가

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,61 +2,163 @@ import 'package:flutter/material.dart';
 import 'package:travel_planner/pages/results_page.dart';
 import 'package:travel_planner/pages/home_page.dart';
 import 'package:travel_planner/main.dart';
+import 'package:provider/provider.dart';
+
+//ChatGPT로 생성한 가상 관광지
+final List<Map<String, dynamic>> pseudoTravelPlan = [{
+      "id": 1,
+      "search_id": 1,
+      "title": "자연과 트레킹",
+      "img1": "assets/images/travel_1.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "신라",
+      "keyword": "자연적",
+      "price": 0.0,
+      "summary": "사르바 항구 산책, 미라노 궁 투어, 라펠라 숲 트레킹"
+    },
+    {
+      "id": 2,
+      "search_id": 1,
+      "title": "호수와 평온함",
+      "img1": "assets/images/travel_2.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "메리어트",
+      "keyword": "평온함",
+      "price": 10.0,
+      "summary": "아르보 바자 탐방, 실비아 호수 유람, 에덴 교회 방문"
+    },
+    {
+      "id": 3,
+      "search_id": 1,
+      "title": "예술과 정원",
+      "img1": "assets/images/travel_3.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "조선",
+      "keyword": "예술적",
+      "price": 40.0,
+      "summary": "나리아 미술관, 로미타 정원 산책, 라빈다 성 투어"
+    },
+    {
+      "id": 4,
+      "search_id": 1,
+      "title": "해변과 여유",
+      "img1": "assets/images/travel_4.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "하얏트",
+      "keyword": "여유로움",
+      "price": 30.0,
+      "summary": "이솔리 해변 수영, 루체르 전망대, 파르니 마을 체험"
+    },
+    {
+      "id": 5,
+      "search_id": 1,
+      "title": "시장과 오솔길",
+      "img1": "assets/images/travel_5.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "쉐라톤",
+      "keyword": "향토적",
+      "price": 15.0,
+      "summary": "라코나 시장 구경, 플로렌 언덕 트레킹, 바르미 오솔길 산책"
+    },
+    {
+      "id": 6,
+      "search_id": 1,
+      "title": "성과 정원",
+      "img1": "assets/images/travel_6.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "콘래드",
+      "keyword": "고풍적",
+      "price": 10.0,
+      "summary": "페르노 성 방문, 사피르 정원 산책, 로살리 박물관"
+    },
+    {
+      "id": 7,
+      "search_id": 1,
+      "title": "예술 여행",
+      "img1": "assets/images/travel_7.jpg",
+      "img2": "assets/images/carousel_1.jpg",
+      "img3": "assets/images/carousel_2.jpg",
+      "hotel": "한화",
+      "keyword": "역사적",
+      "price": 20.0,
+      "summary": "엘토아 해양 박물관, 나리아 성벽 투어, 세라노 거리 예술"
+    }];
+
+
+class TravelPlan {
+  final int id;
+  final int search_id;
+  final String title;
+  final String img1;
+  final String img2;
+  final String img3;
+  final String hotel;
+  final String keyword;
+  final double price;
+  final String summary;
+
+  TravelPlan({
+    required this.id,
+    required this.search_id,
+    required this.title,
+    required this.img1,
+    required this.img2,
+    required this.img3,
+    required this.hotel,
+    required this.keyword,
+    required this.price,
+    required this.summary,
+  });
+}
 
 class SearchPage extends StatefulWidget {
-  //final String command; 
-  final DateTime departure;
-  final DateTime arrival;
-  final String country;
-  final String state;
-  final String city;
-  final int num_people;
-  final double budget;
-  final int accommodation;
-  final List<bool> travel_style;
-
-  SearchPage({
-    required this.departure,
-    required this.arrival,
-    required this.country,
-    required this.state,
-    required this.city,
-    required this.num_people,
-    required this.budget,
-    required this.accommodation,
-    required this.travel_style,
-  });
-
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
+
+
 class _SearchPageState extends State<SearchPage> {
   bool _customTileExpanded = false;
-  List<String> accommodation_labels = ['호텔', '게스트하우스', '리조트', 'Airbnb'];
-  List<String> travel_style_labels = ['휴양 및 힐링', '기념일', '호캉스', '가족여행', '관광','역사 탐방'];
 
-
-//ChatGPT로 생성한 가상 관광지
-  final List<TravelPlan> travel_plans = [
-    TravelPlan(img: 'assets/images/travel_1.jpg', hotel: '신라', keyword: '자연적', name: "#1", price: 0.0, summary: '사르바 항구 산책, 미라노 궁 투어, 라펠라 숲 트레킹'),
-    TravelPlan(img: 'assets/images/travel_2.jpg', hotel: '메리어트', keyword: '평온함', name: "#2", price: 10.0, summary: '아르보 바자 탐방, 실비아 호수 유람, 에덴 교회 방문'),
-    TravelPlan(img: 'assets/images/travel_3.jpg', hotel: '조선', keyword: '예술적', name: "#3", price: 40.0, summary: '나리아 미술관, 로미타 정원 산책, 라빈다 성 투어'),
-    TravelPlan(img: 'assets/images/travel_4.jpg', hotel: '하얏트', keyword: '여유로움', name: "#4", price: 30.0, summary: '이솔리 해변 수영, 루체르 전망대, 파르니 마을 체험'),
-    TravelPlan(img: 'assets/images/travel_5.jpg', hotel: '쉐라톤', keyword: '향토적', name: "#5", price: 15.0, summary: '라코나 시장 구경, 플로렌 언덕 트레킹, 바르미 오솔길 산책'),
-    TravelPlan(img: 'assets/images/travel_6.jpg', hotel: '콘래드', keyword: '고풍적', name: "#6", price: 10.0, summary: '페르노 성 방문, 사피르 정원 산책, 로살리 박물관'),
-    TravelPlan(img: 'assets/images/travel_7.jpg', hotel: '한화', keyword: '역사적', name: "#7", price: 20.0, summary: '엘토아 해양 박물관, 나리아 성벽 투어, 세라노 거리 예술'),
-  ];
+  final List<TravelPlan> travel_plans = pseudoTravelPlan.map((plan) {
+  return TravelPlan(
+    id: plan["id"],
+    search_id: plan["search_id"],
+    title: plan["title"],
+    img1: plan["img1"],
+    img2: plan["img2"], 
+    img3: plan["img3"],
+    hotel: plan["hotel"],
+    keyword: plan["keyword"],
+    price: plan["price"],
+    summary: plan["summary"],
+  );
+  }).toList();
 
   @override
   Widget build(BuildContext context) {
-    String date = '${widget.departure.year.toString()}.${widget.departure.month.toString().padLeft(2, '0')}.${widget.departure.day.toString().padLeft(2, '0')} - ${widget.arrival.year.toString()}.${widget.arrival.month.toString().padLeft(2, '0')}.${widget.arrival.day.toString().padLeft(2, '0')}';
-    String country = widget.country;
-    String state = widget.state;
-    int num_people = widget.num_people;
-    double budget = widget.budget; // 여기서 budget, num_people, accommodation, travel_style 초기화
-    int accommodation = widget.accommodation;
-    List<bool> travel_style = widget.travel_style;
+    final travelInfo = context.watch<UserInputProvider>().userInput;
+
+    if (travelInfo == null) {
+      return Scaffold(
+        appBar: AppBar(
+        title: Text('Search Page', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.cyan[900]),),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.75),
+        ),
+        body: Center(child: Text('Home Page에서 여행 정보를 입력해주세요!')),
+      );
+    }
+
+    String date =
+        '${travelInfo.departure.year.toString()}.${travelInfo.departure.month.toString().padLeft(2, '0')}.${travelInfo.departure.day.toString().padLeft(2, '0')} - ${travelInfo.arrival.year.toString()}.${travelInfo.arrival.month.toString().padLeft(2, '0')}.${travelInfo.arrival.day.toString().padLeft(2, '0')}';
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Search Page', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.cyan[900]),),
@@ -67,7 +169,6 @@ class _SearchPageState extends State<SearchPage> {
           ExpansionTile(
             backgroundColor: Colors.cyan[50],
             collapsedBackgroundColor: Colors.cyan[50],
-            
           title: Column(
             crossAxisAlignment: CrossAxisAlignment.start, 
             children:[
@@ -76,7 +177,6 @@ class _SearchPageState extends State<SearchPage> {
                 children: [
                   SizedBox(width:10),
                   Text('Showing Travel Plans for: ', style:TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black.withOpacity(0.75))), 
-
                 ]
               ),
               SizedBox(height:8)]
@@ -84,11 +184,11 @@ class _SearchPageState extends State<SearchPage> {
           subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start, 
           children: [Row(children:[SizedBox(width:10),Icon(Icons.location_on, color: Colors.cyan[900]!.withOpacity(0.75)),
                                 SizedBox(width: 8.0),
-                                Text('${widget.state}, ${widget.country}', style: TextStyle(fontSize: 14.0)),]), 
+                                Text('${travelInfo.state}, ${travelInfo.country}', style: TextStyle(fontSize: 14.0)),]), 
                                 SizedBox(height:8), 
                     Row(children:[SizedBox(width:10),Icon(Icons.calendar_today, color: Colors.cyan[900]!.withOpacity(0.75)),
                                 SizedBox(width: 8.0),
-                                Text('${widget.departure.year.toString()}.${widget.departure.month.toString().padLeft(2, '0')}.${widget.departure.day.toString().padLeft(2, '0')} - ${widget.arrival.year.toString()}.${widget.arrival.month.toString().padLeft(2, '0')}.${widget.arrival.day.toString().padLeft(2, '0')}', style: TextStyle(fontSize: 14.0)),
+                                Text('${travelInfo.departure.year.toString()}.${travelInfo.departure.month.toString().padLeft(2, '0')}.${travelInfo.departure.day.toString().padLeft(2, '0')} - ${travelInfo.arrival.year.toString()}.${travelInfo.arrival.month.toString().padLeft(2, '0')}.${travelInfo.arrival.day.toString().padLeft(2, '0')}', style: TextStyle(fontSize: 14.0)),
                               ]),SizedBox(height:8),],),
           trailing: Row(
             mainAxisSize: MainAxisSize.min,
@@ -111,23 +211,23 @@ class _SearchPageState extends State<SearchPage> {
             ListTile(title: Column(crossAxisAlignment: CrossAxisAlignment.start, children:[
               Row(children:[SizedBox(width: 10.0),Icon(Icons.person, color: Colors.cyan[900]!.withOpacity(0.75)),
                                 SizedBox(width: 8.0),
-                                Text('인원: ${num_people}명', style: TextStyle(fontSize: 14.0)),
+                                Text('인원: ${travelInfo.numPeople}명', style: TextStyle(fontSize: 14.0)),
                                 SizedBox(width: 16.0),
                                 Icon(Icons.paid, color: Colors.cyan[900]!.withOpacity(0.75)),
                                 SizedBox(width: 8.0),
-                                Text('예산: ${budget}만원', style: TextStyle(fontSize: 14.0)),
+                                Text('예산: ${travelInfo.budget}만원', style: TextStyle(fontSize: 14.0)),
                                 SizedBox(width: 16.0),
                                 Icon(Icons.hotel, color: Colors.cyan[900]!.withOpacity(0.75)),
                                 SizedBox(width: 8.0),
-                                Text('숙소: ${accommodation_labels[accommodation]}', style: TextStyle(fontSize: 14.0)),
+                                Text('숙소: ${accommodationLabels[travelInfo.accommodation]}', style: TextStyle(fontSize: 14.0)),
                                 ]), SizedBox(height:10),
               Row(children:[
-                for (int i = 0; i < travel_style.length; i++)
-                  if (travel_style[i])
+                for (int i = 0; i < travelInfo.travelStyle.length; i++)
+                  if (travelInfo.travelStyle[i])
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 4.0),
                       child: Chip(
-                        label: Text('# ${travel_style_labels[i]}', style: TextStyle(fontSize: 14.0)),
+                        label: Text('${travelStyleLabels[i]}', style: TextStyle(fontSize: 14.0)),
                       ),
                     ),
               ])])),
@@ -150,19 +250,11 @@ class _SearchPageState extends State<SearchPage> {
                     MaterialPageRoute(
                       builder: (context) => ResultsPage(
                         travelPlan: travel_plans[index],
-                        state: state,
-                        budget: budget,
-                        numPeople: num_people,
-                        date: date,
-                        type: accommodation_labels[accommodation],
-                        travel_style: travel_style,
-                        travel_style_labels: travel_style_labels,
-                        country:country,
                       ),
                     ),
                   );
                 },
-              child: TravelPlanCard(travel_plan: travel_plans[index], state: state, budget: budget, num_people: num_people, date: date, type: accommodation_labels[accommodation]),);
+              child: TravelPlanCard(travel_plan: travel_plans[index], state: travelInfo.state, budget: travelInfo.budget, num_people: travelInfo.numPeople, date: date, type: accommodationLabels[travelInfo.accommodation]),);
             },
           ),  
         ),
@@ -201,7 +293,7 @@ class TravelPlanCard extends StatelessWidget {
                 width: 100,
                 height: 100,
                 child: Image.asset(
-                  travel_plan.img, // 이미지 경로 사용
+                  travel_plan.img1, // 이미지 경로 사용
                   fit: BoxFit.cover, // 이미지가 잘 맞도록 설정
                 ),
               ),
@@ -233,7 +325,7 @@ class TravelPlanCard extends StatelessWidget {
                   ),
                   SizedBox(height: 8),
                   // 여행 계획 정보 텍스트
-                  Text('Trip to ${state} ${travel_plan.name}',
+                  Text('Trip to ${state} #${travel_plan.title}',
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 1),
@@ -259,22 +351,4 @@ class TravelPlanCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class TravelPlan {
-  final String img;
-  final String hotel;
-  final String keyword;
-  final String name;
-  final double price;
-  final String summary;
-
-  TravelPlan({
-    required this.img,
-    required this.hotel,
-    required this.keyword,
-    required this.name,
-    required this.price,
-    required this.summary,
-  });
 }
